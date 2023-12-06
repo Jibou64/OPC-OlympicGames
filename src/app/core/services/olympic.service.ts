597d3681637ff02d66705b04104e9e1d7fc86118
BENZEGHIOUA <jibril.benzeghioua@cgi.com>
2023-12-06 16:45:40 +0100

    Correction des erreurs

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
+import { Participation } from '../models/Participation';
+import { ReturnStatement } from '@angular/compiler';
+
 
 @Injectable({
   providedIn: 'root',
@@ -12,20 +16,89 @@ export class OlympicService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Generate datas from our DataBase within the mock
+   *
+   * @param olympicUrl - The array of olympics retrieved by service
+   */
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Array<Olympic>>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
         console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
         return caught;
       })
     );
   }
 
-  getOlympics() {
-    return this.olympics$.asObservable();
+
+  /**
+   * Function to get our data as obersevables
+   *
+   * @param olympics$ - The stream of datas from olympic
+   */
+  getOlympics():Observable<Array<Olympic>> {
+    return this.olympics$.asObservable()
   }
+
+
+
+   /**
+   * Function to get our number of medals
+   *
+   * @param participation - The stream of datas from participation
+   */
+  countMedals(olympic: Olympic): number {
+    let medals: number = 0;
+
+    for (let participation of olympic.participations){
+      medals += participation.medalsCount
+    }
+    return medals
+  }
+
+
+   /**
+   * Function to get our number of athletes
+   *
+   * @param participations - The stream of datas from olympic.participations
+   * 
+   */
+  numberParticipants(olympic: Olympic): number {
+    let athletes = 0;
+    let idAthlete = 0;
+
+    for (let athlete of olympic.participations){
+      athletes = athlete.athleteCount
+    }
+    return athletes
+  }
+
+   /**
+   * Function to get our number of games
+   *
+   * @param olympics - The stream of datas from olympic
+   * 
+   */
+  countGames(olympics: Array<Olympic> | null): number {
+    var setOlympic = new Set<number>();
+    if (olympics){
+      for(let olympic of olympics){
+        for(let participation of olympic.participations){
+          setOlympic.add(participation.year)
+        }
+      }
+    return setOlympic.size;
+    }
+    else {
+      return 0;
+    }
+  }
+
+
+
+
+  
+
 }

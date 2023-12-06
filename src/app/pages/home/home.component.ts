3bf02adc677ac3f599895dae32eaaab3ee1d4012
BENZEGHIOUA <jibril.benzeghioua@cgi.com>
2023-12-06 16:36:59 +0100

    Ajout OnInit / OnDestroy HomePage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { interval, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { CountryComponent } from '../country/country.component';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public olympics$: Observable<any> = of(null);
+  pieChart!: any;
+  numberOfLabels: Array<string> = [];
+  numberOfMedals: Array<number> = [];
+  numberOfGames: number = 0;
+  subscription!: Subscription;
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService, private router: Router) {}
+
+
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.subscription = this.olympics$.subscribe((value) => {
+      this.modifyChartData(value);
+    });
+  }
+
+  
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
